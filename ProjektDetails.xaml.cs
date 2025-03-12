b0fee0877a8e3aab26d168a048b80ee73af10dd2
Cris <cckaiser2563@gmail.com>
2025-03-12 11:28:21 +0100

    Draggable Background

--- ProjektDetails.xaml.cs
+++ ProjektDetails.xaml.cs
@@ -3,18 +3,59 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media;
+using static System.Windows.Forms.AxHost;
 
 namespace AbstractApp
 {
     public partial class ProjektDetails : Window
     {
         private Point _clickPosition; 
+        
+        private bool isDragging = false;
+        private Point startDragPoint;
+        private double startX, startY;
 
         public ProjektDetails(Projekt projekt)
         {
             InitializeComponent();
             ProjektNameText.Text = $"Projekt: {projekt.Name}";
             ErstellungsdatumText.Text = $"Erstellt am: {projekt.Erstellungsdatum.ToShortDateString()}";
+        }
+
+        private void Grid_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Middle)
+            {
+                isDragging = true;
+                startDragPoint = e.GetPosition(this); // Position relativ zum Fenster
+                startX = GridTransform.X;
+                startY = GridTransform.Y;
+                ((Grid)sender).CaptureMouse();
+                e.Handled = true;
+            }
+        }
+
+        private void Grid_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDragging)
+            {
+                Point currentPoint = e.GetPosition(this);
+                GridTransform.X = startX + (currentPoint.X - startDragPoint.X);
+                GridTransform.Y = startY + (currentPoint.Y - startDragPoint.Y);
+
+                // Hintergrund neu zeichnen
+                DraggableGrid.InvalidateVisual();
+            }
+        }
+
+        private void Grid_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Middle)
+            {
+                isDragging = false;
+                ((Grid)sender).ReleaseMouseCapture();
+                e.Handled = true;
+            }
         }
 
         private void BtnZurueck_Click(object sender, RoutedEventArgs e)
@@ -26,9 +67,14 @@ namespace AbstractApp
 
         private void Grid_MouseRightButtonDown(object sender, MouseButtonEventArgs e)
         {
-            _clickPosition = e.GetPosition(this); 
+            _clickPosition = e.GetPosition(DraggableGrid);
         }
 
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            GridTransform.X = -1000;
+            GridTransform.Y = -1000;
+        }
 
         private void MenuItem_EintragHinzufuegen_Click(object sender, RoutedEventArgs e)
         {
@@ -56,22 +102,7 @@ namespace AbstractApp
                 textBox.Height = (lineCount * lineHeight) + textBox.Padding.Top + textBox.Padding.Bottom;
             };
 
-            ((Grid)this.Content).Children.Add(textBox);
-        }
-
-        private void MenuItem_NeuerCluster_Click(object sender, RoutedEventArgs e)
-            {
-                MessageBox.Show("Neuer Cluster wird erstellt...");
-            }
-
-        private void MenuItem_ClusterBearbeiten_Click(object sender, RoutedEventArgs e)
-        {
-            MessageBox.Show("Cluster wird bearbeitet...");
-        }
-
-        private void MenuItem_ClusterLoeschen_Click(object sender, RoutedEventArgs e)
-        {
-            MessageBox.Show("Cluster wird gelöscht...");
+            DraggableGrid.Children.Add(textBox);
         }
 
         private void MenuItem_EintragBearbeiten_Click(object sender, RoutedEventArgs e)
